2ba9989143e970b2147187b90db8aaebcabc0ddd
sehyun <germs1020@gmail.com>
2024-01-17 04:13:57 +0900

    init: init files

---
+++ JPA-test/src/test/java/com/ssafy/jpatest/JpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.jpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
